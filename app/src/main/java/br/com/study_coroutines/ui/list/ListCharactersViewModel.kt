89a823e13dfb6333ad28b6fb80f89960deb4da7b
João Gabriel <gsantana152@gmail.com>
2020-06-13 15:10:36 -0300

    removed commented lines

--- app/src/main/java/br/com/study_coroutines/ui/list/ListCharactersViewModel.kt
+++ app/src/main/java/br/com/study_coroutines/ui/list/ListCharactersViewModel.kt
@@ -1,46 +1,20 @@
 package br.com.study_coroutines.ui.list
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import br.com.study_coroutines.data.PersonageDataSource
 import br.com.study_coroutines.domain.repository.CharacterRepository
-import br.com.study_coroutines.network.Status
-import br.com.study_coroutines.ui.ViewAction
-import kotlinx.coroutines.launch
 
 class ListCharactersViewModel(private val repository: CharacterRepository) : ViewModel() {
 
-    // val viewState = ViewState()
     val allPersonages = Pager(
         PagingConfig(
             pageSize = 20,
             enablePlaceholders = true,
             maxSize = 100
         )
-    ){
+    ) {
         PersonageDataSource(repository)
     }.flow
-
-    // fun dispatchAction(action: ViewAction) = when (action) {
-    //     is ViewAction.Init -> getPosts()
-    //     is ViewAction.Refresh -> getPosts()
-    // }
-
-    // private fun getCharacters() {
-    //     viewModelScope.launch {
-    //         val result = repository.getCharacters(1)
-    //
-    //         when (result.status) {
-    //             Status.SUCCESS -> {
-    //                 charactersLiveData.value?.addAll(result.data ?: emptyList())
-    //             }
-    //             Status.ERROR -> {
-    //
-    //             }
-    //         }
-    //     }
-    // }
-
 }
